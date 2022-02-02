e4a7b87991937d0bcd7739b65f3ff8cd63710e69
achusky <chuxiaojie@megvii.com>
2022-02-03 00:18:54 +0800

    fix missing import for text.py

--- basicsr/test.py
+++ basicsr/test.py
@@ -10,10 +10,9 @@ from os import path as osp
 
 from basicsr.data import create_dataloader, create_dataset
 from basicsr.models import create_model
-from basicsr.train import parse_options
 from basicsr.utils import (get_env_info, get_root_logger, get_time_str,
                            make_exp_dirs)
-from basicsr.utils.options import dict2str
+from basicsr.utils.options import dict2str, parse_options
 
 
 def main():
